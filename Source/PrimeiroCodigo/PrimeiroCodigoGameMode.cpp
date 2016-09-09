c23f0694d77c6eb2902af6e30d717c1fb7098aa9
alexsantosfilho <tudomaistv@gmail.com>
2016-09-09 18:42:51 -0300

    Primeiro commit pra sa

---
+++ Source/PrimeiroCodigo/PrimeiroCodigoGameMode.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PrimeiroCodigo.h"
+#include "PrimeiroCodigoGameMode.h"
+#include "MyHUD.h"
+
+APrimeiroCodigoGameMode::APrimeiroCodigoGameMode() {
+
+	HUDClass = AMyHUD::StaticClass();
+
+}
+
+
---
+++ Source/PrimeiroCodigo/PrimeiroCodigoGameMode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PrimeiroCodigoGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRIMEIROCODIGO_API APrimeiroCodigoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+		APrimeiroCodigoGameMode();
+	
+	
+};
