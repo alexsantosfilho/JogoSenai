b7331f17a930d4e0f255bb4a8a1762fd5885dfd0
Alexsandro Giovanni dos Santos Filho <alexsandro_giovanni@estudante.sc.senai.br>
2016-09-21 17:17:13 -0300

    atualização do cenario

--- Source/MyProject2/Ptrovao.cpp
+++ Source/MyProject2/Ptrovao.cpp
@@ -30,7 +30,7 @@ APtrovao::APtrovao()
 		(TEXT("Particle"));
 	Particle->bAutoActivate = true;
 	static ConstructorHelpers::FObjectFinder<UParticleSystem>
-		ParticleSystem(TEXT("ParticleSystem'/Game/FX_Monsters/FX_Monster_Gruntling/Lightning/P_GruntLightning_gun_01.P_GruntLightning_gun_01'"));
+		ParticleSystem(TEXT("ParticleSystem'/Game/InfinityBladeEffects/Effects/FX_Mobile/Lightning/P_LineToPoint_Blast_Lightning_00.P_LineToPoint_Blast_Lightning_00'"));
 	if (ParticleSystem.Succeeded()) {
 		Particle->SetTemplate(ParticleSystem.Object);
 	}
