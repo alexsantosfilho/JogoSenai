a2a596f34be95cd3d733ee017966f7232b8f9b1e
alexsantosfilho <tudomaistv@gmail.com>
2016-09-27 10:19:40 -0300

    teste

--- Source/MyProject2/Lixeira.cpp
+++ Source/MyProject2/Lixeira.cpp
@@ -10,7 +10,7 @@ ALixeira::ALixeira()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+//
 	Root = CreateDefaultSubobject<UBoxComponent>(TEXT("Root"));
 	Root->SetCollisionProfileName("BlockAllDynamic");
 	RootComponent = Root;
